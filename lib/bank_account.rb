4188c23d900625130ac2c722667279a128c34611
Ad Friedman <adam.friedman@flatironschool.com>
2019-04-04 13:40:52 -0400

    changes

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
 
+attr_reader :name, :balance, :status
+attr_accessor :balance, :status
+
+def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+end
+
+
+def deposit(cash_amount)
+  @balance += cash_amount
+end
+
+def display_balance
+  "Your balance is $#{@balance}."
+end
+
+def valid?
+  if @status == "open" and @balance > 0
+    true
+  else
+    false
+  end
+end
+
+def close_account
+  @status = "closed"
+
+end
+
+
 end
